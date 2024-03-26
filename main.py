79a49560c7ebdd1f465e8a3a4adb170b529d36f2
3022-2 <3022-2@gmail.com>
2024-03-26 00:32:05 +0000

    changes to main

--- main.py
+++ main.py
@@ -1,7 +1,51 @@
+from colorama import Fore, init
 
+import customtkinter
+import os
+import time
+
+cwd = os.getcwd()
+
+init(autoreset=True)
+
+#pre code checks
+try:
+    if os.path.exists(f"{cwd}\\content") == False:
+        os.mkdir(f"{cwd}\\content")
+        time.sleep(1)
+        os.mkdir(f"{cwd}\\content\\youtube")
+        os.mkdir(f"{cwd}\\content\\youtube\\videos")
+        os.mkdir(f"{cwd}\\content\\youtube\\playlists")
+        os.mkdir(f"{cwd}\\content\\youtube\\channels")
+        os.mkdir(f"{cwd}\\content\\spotify")
+        os.mkdir(f"{cwd}\\content\\spotify\\songs")
+        os.mkdir(f"{cwd}\\content\\spotify\\albums")
+        os.mkdir(f"{cwd}\\content\\spotify\\playlists")
+    elif os.path.exists(f"{cwd}\\content") == True:
+        pass
+    else:
+        print(Fore.RED + "Error")
+except Exception as e:
+    print(Fore.RED + e)
+
+#defining vars
+youtube_videos_folder = f"{cwd}\\content\\youtube\\videos"
+youtube_playlists_folder = f"{cwd}\\content\\youtube\\playlists"
+youtube_channels_folder = f"{cwd}\\content\\youtube\\channels"
+
+spotify_songs_folder = f"{cwd}\\content\\spotify\\songs"
+spotify_albums_folder = f"{cwd}\\content\\spotify\\albums"
+spotify_playlists_folder = f"{cwd}\\content\\spotify\\playlists"
+
+def spotify():
+    pass
+
+def youtube():
+    pass
 
 def main():
-    pass
+    root = customtkinter.CTk()
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
