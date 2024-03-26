5b895b9f23ffae5bf1a032f694e17fd5320496a4
3022-2 <3022-2@gmail.com>
2024-03-26 01:33:49 +0000

    added more code

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from colorama import Fore, init
 from CTkMessagebox import CTkMessagebox
+from colorama import Fore, init
 
 import customtkinter
-import os
+import webbrowser
 import time
+import os
 
 cwd = os.getcwd()
 
@@ -30,7 +31,7 @@ except Exception as e:
     print(Fore.RED + e)
 
 #defining vars
-icon = f"{cwd}\\icon.ico" #icon from https://icon-icons.com/
+icon= f"{cwd}\\icon.ico" #icon from https://icon-icons.com/
 
 youtube_videos_folder = f"{cwd}\\content\\youtube\\videos"
 youtube_playlists_folder = f"{cwd}\\content\\youtube\\playlists"
@@ -47,10 +48,34 @@ def youtube():
     pass
 
 def main():
+    customtkinter.set_appearance_mode("dark")
     root = customtkinter.CTk()
     root.minsize(480, 270)
+    root.maxsize(480, 270)
     root.title("spotitube")
-    root.iconbitmap(icon)
+
+    try:
+        root.iconbitmap(icon)
+    except Exception as e:
+        print(Fore.YELLOW + "icon.ico not found, continuing")
+        
+    root.update()
+    window_width = root.winfo_width()
+
+    top_frame = customtkinter.CTkFrame(master=root, width=window_width, height=50)
+    top_frame.pack(padx=10, pady=20)
+
+    customtkinter.CTkLabel(master=top_frame, text="spotitube gui witten by github.com/3022-2/", text_color="grey",font=("", 20), width=window_width, height=50).pack()
+
+    youtube_button = customtkinter.CTkButton(master=root, command=lambda: youtube, text="YouTube")
+    youtube_button.pack(padx=10, pady=0)
+    spotify_button = customtkinter.CTkButton(master=root, command=lambda: spotify, text="Spotify")
+    spotify_button.pack(padx=10, pady=10)
+    github_button = customtkinter.CTkButton(master=root, command=lambda: webbrowser.open("https://github.com/3022-2/spotitube-gui"), text="GitHub")
+    github_button.pack(padx=10, pady=0)
+    exit_button = customtkinter.CTkButton(master=root, command=lambda: exit(), text="Exit")
+    exit_button.pack(padx=10, pady=10)
+
     root.mainloop()
 
 if __name__ == "__main__":
